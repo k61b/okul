3a9a022ec87b478eadc14ed5e7115687077d7e87
Kayra Berk Tuncer <kayrabtuncer@gmail.com>
2023-08-08 10:02:39 +0300

    chore: update migrate sql file

--- backend/migrations/001_create_tables.sql
+++ backend/migrations/001_create_tables.sql
@@ -1,7 +1,14 @@
--- migrations/001_create_tables.sql
-
 -- Create the 'users' table
 CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+    updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+-- Create the 'admins' table
+CREATE TABLE IF NOT EXISTS admins (
     id SERIAL PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
@@ -15,15 +22,6 @@ CREATE TABLE IF NOT EXISTS schools (
     name VARCHAR(255) NOT NULL,
     description TEXT,
     owner_id INT REFERENCES users(id) ON DELETE CASCADE,
-    created_at TIMESTAMPTZ DEFAULT NOW(),
-    updated_at TIMESTAMPTZ DEFAULT NOW()
-);
-
--- Create the 'admins' table
-CREATE TABLE IF NOT EXISTS admins (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL,
-    password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMPTZ DEFAULT NOW(),
     updated_at TIMESTAMPTZ DEFAULT NOW()
 );
