77687512ddbff44da4cadf4dfcb07fccb45a0f77
Kayra Berk Tuncer <kayrabtuncer@gmail.com>
2023-08-08 10:59:12 +0300

    chore: update config folder

--- backend/config/config_test.go
+++ backend/config/config_test.go
@@ -1,38 +1,82 @@
 package config
 
 import (
-	"os"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestLoadConfig(t *testing.T) {
-	// Set environment variable for the config file path
-	os.Setenv("CONFIG_FILE", "config.yaml")
+	tests := []struct {
+		name        string
+		env         string
+		expectedErr bool
+	}{
+		{
+			name:        "Load Dev Config",
+			env:         "dev",
+			expectedErr: false,
+		},
+		{
+			name:        "Load Prod Config",
+			env:         "prod",
+			expectedErr: false,
+		},
+		{
+			name:        "Invalid Config",
+			env:         "invalid_env",
+			expectedErr: true,
+		},
+	}
 
-	cfg, err := LoadConfig()
-	assert.NoError(t, err)
-
-	// Test values from the example config.yaml
-	assert.Equal(t, "your-db-username", cfg.GetString("database.username"))
-	assert.Equal(t, "your-db-password", cfg.GetString("database.password"))
-	assert.Equal(t, "localhost", cfg.GetString("database.host"))
-	assert.Equal(t, 5432, cfg.GetInt("database.port"))
-	assert.Equal(t, "your-db-name", cfg.GetString("database.name"))
-
-	assert.Equal(t, 8080, cfg.GetInt("server.port"))
-
-	assert.Equal(t, "your-jwt-secret-key", cfg.GetString("jwt.secret_key"))
-	assert.Equal(t, time.Hour*24, cfg.GetDuration("jwt.expiration"))
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Load the configuration
+			cfg, err := LoadConfig(tt.env)
+			if tt.expectedErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+				assert.NotNil(t, cfg)
+			}
+		})
+	}
 }
 
-func TestInvalidConfigFile(t *testing.T) {
-	// Set environment variable for a non-existent config file
-	os.Setenv("CONFIG_FILE", "nonexistent.yaml")
+func TestValidateConfig(t *testing.T) {
+	tests := []struct {
+		name        string
+		config      Config
+		expectedErr bool
+	}{
+		{
+			name: "Valid Configuration",
+			config: Config{
+				Server:   ServerConfig{Port: "8080"},
+				Database: DatabaseConfig{Host: "localhost", Port: "5432", User: "user", Password: "pass", Name: "db"},
+			},
+			expectedErr: false,
+		},
+		{
+			name:        "Invalid Server Port",
+			config:      Config{Server: ServerConfig{Port: ""}, Database: DatabaseConfig{Host: "localhost"}},
+			expectedErr: true,
+		},
+		{
+			name:        "Invalid Database Host",
+			config:      Config{Server: ServerConfig{Port: "8080"}, Database: DatabaseConfig{Host: ""}},
+			expectedErr: true,
+		},
+	}
 
-	cfg, err := LoadConfig()
-	assert.Error(t, err)
-	assert.Nil(t, cfg)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := ValidateConfig(&tt.config)
+			if tt.expectedErr {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
 }
