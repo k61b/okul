41a8569a455ad365c21e583e00f18ca2352e9993
Yusuf Akçekoce <94628640+yusufakcekoce@users.noreply.github.com>
2023-08-20 16:41:34 +0300

    feat: added global popup component (#39)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,14 @@ import {
   CardHeader,
   CardTitle,
 } from './components/ui/card'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './components/ui/dialog'
 
 function App() {
   return (
@@ -51,6 +59,21 @@ function App() {
           <p>Card Footer</p>
         </CardFooter>
       </Card>
+
+      <div className="mt-7">
+        <Dialog>
+          <DialogTrigger>Open</DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Are you sure absolutely sure?</DialogTitle>
+              <DialogDescription>
+                This action cannot be undone. This will permanently delete your
+                account and remove your data from our servers.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
     </div>
   )
 }
